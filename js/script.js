09c0fa05b91e27456bd48b01422c51ccac3675f7
ChriEffe <christianferrazzano93@gmail.com>
2021-12-03 15:37:36 +0100

    stampo numeri nel DOM

--- js/script.js
+++ js/script.js
@@ -7,3 +7,38 @@ Consigli del giorno:
 * Dividete in piccoli problemi la consegna.
 * Individuate gli elementi di cui avete bisogno per realizzare il programma.*/
 
+
+// array con 5 numeri casuali
+let array = arrayRand(5)
+// lo stampo in pagina
+document.writeln(array);
+
+
+
+
+
+
+
+// funzione per creare array da 5 numeri
+function arrayRand(numb) {
+    // creo array vuoto
+    const array = [];
+    // ciclo per creare numeri random
+    for (let i = 0; i < numb; i++) {
+        let numbRand = getRndInteger(1, 99);
+        // ciclo per non duplicare i numeri
+        while (array.includes(numbRand)) {
+            numbRand = getRndInteger(1, 99);
+        }
+        // pusho i numeri nell'array
+        array.push(numbRand);
+    }
+    return array
+}
+
+// genero numeri casuali
+function getRndInteger(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
